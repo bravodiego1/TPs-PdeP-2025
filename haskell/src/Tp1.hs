1a4bc2ce2180889b799d3a0385acf5312da3b160
annkizz <antonellajus2005@gmail.com>
2025-05-05 22:01:59 -0300

    Ejercicio 2.a) estadoDeSaludDelAuto realizado y verificado en los comentarios

--- haskell/src/Tp1.hs
+++ haskell/src/Tp1.hs
@@ -2,10 +2,14 @@ module Tp1 where
 import Text.Show.Functions()
 
 --Punto 1
+
+type Ruedas = Int
+type Chasis = Int
+
 data Auto = UnAuto {
     marca :: String,
     modelo :: String,
-    desgaste :: (Int, Int),
+    desgaste :: (Ruedas, Chasis),
     velocidadMaxima :: Int,
     tiempoDeCarrera :: Int,
     apodos :: [String]
@@ -46,7 +50,7 @@ fiat = UnAuto {
     modelo = "600",
     desgaste = (27, 33),
     velocidadMaxima = 44,
-    tiempoDeCarrera = 0,
+    tiempoDeCarrera = 0 ,
     apodos = ["La Bocha", "La Bolita", "Fitito"]
 }
 
@@ -60,3 +64,30 @@ peugeot = UnAuto {
     apodos = ["El rey del desierto"]
 } 
 
+{- Saber si un auto está en buen estado, lo que podemos determinar de la siguiente manera:
+Los Peugeot no están jamás en buen estado (basado en algún hecho real). 
+Si es de otra marca, 
+Si tiene una cantidad de segundos en carrera menor a 100, se determina en buen estado cuando el desgaste de chasis es menor a 20. 
+En caso contrario, cuando el desgaste del chasis es menor a 40 y el de las ruedas es menor a 60. -}
+
+estadoDeSaludDelAuto :: Auto -> String 
+estadoDeSaludDelAuto unAuto 
+  | ((=="Peugeot") . marca) unAuto = "No esta en buen estado"
+  | tiempoDeCarrera unAuto >= 100 && ((<40) . snd . desgaste) unAuto && ((<60) . fst . desgaste) unAuto = "Esta en buen estado"
+  | tiempoDeCarrera unAuto < 100 &&  ((<20) . snd . desgaste) unAuto = "Esta en buen estado"
+  | otherwise = "No esta en buen estado"
+
+{- 
+> estadoDeSaludDelAuto (UnAuto "Peugeot" "504" (0,0) 40 0 ["El rey del desierto"])
+"No esta en buen estado."
+> estadoDeSaludDelAuto (UnAuto "Lamborghini" "Diablo" (0,7) 73 99 ["Lambo", "La bestia"])
+"Esta en buen estado"
+> estadoDeSaludDelAuto (UnAuto "Fiat" "600" (0,33) 44 99 ["La Bocha", "La Bolita", "Fitito"])
+"No esta en buen estado"
+> estadoDeSaludDelAuto (UnAuto "Ferrari" "F50" (50,30) 65 130 ["La nave", "El fierro", "Ferrucho"])
+"Esta en buen estado"
+> estadoDeSaludDelAuto (UnAuto "Ferrari" "F50" (50, 45) 65 15 ["La nave", "El fierro", "Ferrucho"])
+"No esta en buen estado"
+> estadoDeSaludDelAuto (UnAuto "Ferrari" "F50" (70,30) 65 150 ["La nave", "El fierro", "Ferrucho"])
+"No esta en buen estado"
+-}
